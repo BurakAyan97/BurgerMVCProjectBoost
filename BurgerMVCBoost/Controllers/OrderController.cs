960d729936a2f601d4a7eb97b38f7a0581dfddd4
halitozturk <halit-ozturk@outlook.com.tr>
2023-04-04 17:09:33 +0300

    updated

--- BurgerMVCBoost/Controllers/OrderController.cs
+++ BurgerMVCBoost/Controllers/OrderController.cs
@@ -1,13 +1,34 @@
-﻿using Microsoft.AspNetCore.Authorization;
+﻿using BurgerMVC.BusinessLayer.Concrete;
+using BurgerMVC.DataLayer.Concrete;
+using BurgerMVC.DataLayer.EntityFramework;
+using BurgerMVCBoost.ViewModel;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 
 namespace BurgerMVCBoost.Controllers
 {
-    [Authorize]
+    //[Authorize]
     public class OrderController : Controller
     {
+        MenuManager menuManager = new MenuManager(new EfMenuDal());
+        ExtraManager extraManager = new ExtraManager(new EfExtraDal());
+        DessertManager dessertManager = new DessertManager(new EfDessertDal());
+        SauceManager sauceManager = new SauceManager(new EfSauceDal());
+        DrinkManager drinkManager = new DrinkManager(new EfDrinkDal());
         public IActionResult OrderList()
         {
+            AllMenuVM vm = new AllMenuVM()
+            {
+                Extras = extraManager.TGetList(),
+                Desserts = dessertManager.TGetList(),
+                Sauces = sauceManager.TGetList(),
+                Menus = menuManager.TGetList(),
+                Drinks = drinkManager.TGetList(),
+            };
+            return View(vm);
+        }
+        public IActionResult Order() 
+        { 
             return View();
         }
     }
